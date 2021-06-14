d9c73f7a88c57424ed0c38617a046520c38a3e07
Robert Wang <rwang5688@outlook.com>
2021-06-13 23:02:03 -0700

    remove cp-lambda-function-python.sh

--- lambda-function-python/create.sh
+++ lambda-function-python/create.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-cd src
-./cp-lambda-function-python.sh
+rm -rf ./dist && mkdir dist
+cp -r ./src/* ./dist
+cd ./dist
+zip -r lambda-function-python.zip .
+aws s3 cp lambda-function-python.zip s3://wangrob-lambda-function-python
+cd ..
 
-cd ../templates
+cd templates
 ./create-lambda-function-python.sh
 
 cd ..
--- lambda-function-python/src/cp-lambda-function-python.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cp *.py requirements.txt ../dest
-cd ../dest
-zip -r lambda-function-python.zip .
-aws s3 cp lambda-function-python.zip s3://wangrob-lambda-function-python
-cd ../src
-
